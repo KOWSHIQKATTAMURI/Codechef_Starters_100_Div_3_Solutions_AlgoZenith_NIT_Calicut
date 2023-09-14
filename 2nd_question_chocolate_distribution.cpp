d8281aae53a0c77eb76af5b7d48e1f1ee2d11ff2
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-09-14 05:50:35 +0530

    Update 2nd_question_chocolate_distribution.cpp

--- 2nd_question_chocolate_distribution.cpp
+++ 2nd_question_chocolate_distribution.cpp
@@ -7,6 +7,10 @@ int main(){
     while(t--){
         int n;
         cin >> n;
+        
+        //there are only 3 jars
+        //so if two of them have one chocolate each, then all the conditions will be satisfied
+        //so the remaining jar can have the rest of the chocolates
         cout << 1 << " " << 1 << " " << n - 2 << endl;
     }
     return 0;
